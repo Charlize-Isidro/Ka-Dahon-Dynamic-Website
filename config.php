ea06cbbb35262bda419838c8abcfa7f94be5edc8
Charlize Isidro <isidroca@students.nu-baliwag.edu.ph>
2023-05-29 17:09:19 +0800

    launching

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
+$server = "localhost";
+$server_username = "u235219407_kadahon";
+$server_password = "CharKadahon123";
+$server_database = "u235219407_kadahonshop";
+$home = "https://ka-dahon.shop/";
 
-$conn = mysqli_connect('localhost','root','','project') or die('connection failed');
+$conn = mysqli_connect($server, $server_username, $server_password, $server_database);
 
 ?>
