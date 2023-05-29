82cf74e3c2fba3218515e3c7403d6690445fdbbc
Charlize Isidro <isidroca@students.nu-baliwag.edu.ph>
2023-05-29 17:11:59 +0800

    query

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $server = "localhost";
 $server_username = "u235219407_kadahon";
 $server_password = "CharKadahon123";
 $server_database = "u235219407_kadahonshop";
-$home = "https://ka-dahon.shop/";
+$home = "https://website.ka-dahon.shop/";
 
 $conn = mysqli_connect($server, $server_username, $server_password, $server_database);
 
